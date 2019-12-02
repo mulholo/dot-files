ac38758433a89e6da42bb8b94e7a6ebe0304ab3b
mulholio <james@jmulholland.com>
2019-12-02 14:45:03 +0000

    add yarn option to get-repo

--- bin/get-repo.sh
+++ bin/get-repo.sh
@@ -4,7 +4,16 @@
 # use . or source before the script to make the cd work in your current context
 # usage: . get-repo zeit next.js
 
-org=$1
-repo_name=$2
+cmd='cd ~/proj/ && git clone "https://github.com/${org}/${repo_name}" && cd $repo_name'
 
-cd ~/proj/ && git clone "https://github.com/${org}/${repo_name}" && cd $repo_name
+# yarn install if -y flag is passed
+if [[ $1 = '-y' ]]; then
+  org=$2
+  repo_name=$3
+  yarn_cmd="${cmd} && yarn install"
+  eval $yarn_cmd
+else
+  org=$1
+  repo_name=$2
+  eval $cmd
+fi
