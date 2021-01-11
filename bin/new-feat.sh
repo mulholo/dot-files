26a17f69a9cd1f24e39a658a6083dd03d9c8c2f0
mulholio <james@jmulholland.com>
2021-01-11 11:25:44 +0000

    update new-feat script for better error handling and ergonomics

--- bin/new-feat.sh
+++ bin/new-feat.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Starts a new feature branch to match the Jira
-# e.g. new-feat 1234 write-new-feat-script
+# Starts a new feature branch to match the Jira ticket number
+# e.g. new-feat 1234 "write new feat script" -> git checkout -b WEBSITE-1234-write-new-feat-script
 
 jira_project="WEBSITE"
 number=$1
 description=$2
-branch_name="${jira_project}-${number}-${description}"
 
-cd ~/proj/memrise && git checkout master && git pull && git checkout -b $branch_name
-
+if [[ -z "$number" ]]; then
+  echo "no number passed"
+elif [[ -z "$description" ]]; then
+  echo "no description passed"
+else
+  hyphenated_description_=${description// /-}
+  branch_name="${jira_project}-${number}-${hyphenated_description_}"
+  cd ~/proj/memrise && git checkout master && git pull && git checkout -b $branch_name
+fi
