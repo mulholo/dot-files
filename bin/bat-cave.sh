9fdfc7c8c676c2d3eebd7e045c473d03ca130980
mulholio <james@jmulholland.com>
2021-01-11 11:26:28 +0000

    del bat-cave script in favor of tmuxinator

--- bin/bat-cave.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# generic script to start a new dev tmux session
-tmux new-session -d -s 'dev' -n '🎛️  ' # put commandsat the end here as appropriatee.g. 'vim'
-tmux split-window -v
-tmux new-window -n '👨‍💻  '
-tmux attach-session -d
