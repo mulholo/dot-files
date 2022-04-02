cc070b220a966747f5a73a41f1e958015bf09c6c
James Mulholland <james@attio.com>
2022-04-02 21:50:13 +0100

    tweak options

--- nvim/lua/mulholo/options.lua
+++ nvim/lua/mulholo/options.lua
@@ -1,7 +1,11 @@
 -- :help options
-vim.g.mapleader = ' '               -- Set <leader> to ' '
-vim.opt.list = true                 -- Show &nbsp and tabs. Great for avoiding trailing whitespace and broken md files
-vim.opt.cursorline = true           -- show which column the cursor is in
-vim.opt.relativenumber = true       -- Set relative line number and current line number
-vim.opt.confirm = true              -- Ask what to do about unsaved/read-only files
 vim.cmd "filetype plugin indent on" -- Enable file type detection and language-dependent indenting.
+vim.g.mapleader = ' '               -- Set <leader> to ' '
+vim.opt.confirm = true              -- Ask what to do about unsaved/read-only files
+vim.opt.cursorline = true           -- show which column the cursor is in
+vim.opt.list = true                 -- Show &nbsp and tabs. Great for avoiding trailing whitespace and broken md files
+vim.opt.mouse = "a"                 -- Allow mouse usage
+vim.opt.relativenumber = true       -- Set relative line number and current line number
+vim.opt.signcolumn = "yes"          -- Always show sign column to avoid shifting
+vim.opt.undofile = true             -- Allow persistant undo
+vim.opt.scrolloff = 4               -- Preserve more context around cursor when scrolling
