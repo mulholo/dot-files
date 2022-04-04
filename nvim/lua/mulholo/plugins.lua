e7591a13aa5c5923d00051d6e747de5e87fdae16
James Mulholland <james@attio.com>
2022-04-04 21:23:38 +0100

    install completion plugins and reorganise plugins list

--- nvim/lua/mulholo/plugins.lua
+++ nvim/lua/mulholo/plugins.lua
@@ -45,25 +45,28 @@ return packer.startup(function(use)
   use 'nvim-telescope/telescope.nvim'       -- (Fuzzy) finder
   use 'tpope/vim-surround'                  -- Select surrounding (, <tag>, ' etc.
   use 'tpope/vim-repeat'                    -- Extend .'s behaviour
+  use "L3MON4D3/LuaSnip"                    -- Snippet engine
+  use 'lifepillar/vim-solarized8'           -- Solarized theme
+  use 'vim-airline/vim-airline'             -- Airline
+  use 'vim-airline/vim-airline-themes'      -- Airline themes
+  use 'edkolev/tmuxline.vim'                -- Airline look for TMUX
+  -- use "nvim-lua/popup.nvim"              -- An implementation of the Popup API from vim in Neovim
+
+  -- Completion
+  use "hrsh7th/nvim-cmp"                    -- Completion
+  use "hrsh7th/cmp-buffer"                  -- Buffer Completion
+  use "hrsh7th/cmp-path"                    -- Path Completion
+  use "hrsh7th/cmp-cmdline"                 -- Commandline Completion
+  use "saadparwaiz1/cmp_luasnip"            -- Completion
+
+  -- Motions and text objects
+  use 'michaeljsmith/vim-indent-object'     -- Add text object for lines: ai -> and line above; ii -> no line above; aI -> line above and line below
   use 'tpope/vim-commentary'                -- Comment motion e.g. gcip to comment a paragraph
   use 'vim-scripts/ReplaceWithRegister'     -- Replace motion e.g. `gre` to replace to end of word with current buffer
   use 'christoomey/vim-sort-motion'         -- Sort motion
   use 'kana/vim-textobj-user'               -- Enable other text object plugins
   use 'Julian/vim-textobj-variable-segment' -- Add text object for parts of camel and snake-case variables e.g. foo_ba|r_baz -> civquux -> foo_quux_baz
 
-  -- Add text object for lines
-  -- ai -> indentation level and line above
-  -- ii -> indentation level, no line above
-  -- aI -> indentation level, line above and line below
-  use 'michaeljsmith/vim-indent-object'
-
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'edkolev/tmuxline.vim'
-
-  use 'lifepillar/vim-solarized8'
-
-  -- use "nvim-lua/popup.nvim"  -- An implementation of the Popup API from vim in Neovim
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
