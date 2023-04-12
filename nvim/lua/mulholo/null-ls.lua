d4cbf205290e59a2576b513a21eb2a3ea2a09256
James Mulholland <james@attio.com>
2023-04-12 13:11:19 +0100

    fix nvim config after upgrade

--- nvim/lua/mulholo/null-ls.lua
+++ nvim/lua/mulholo/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 local code_actions = null_ls.builtins.diagnostics
@@ -9,27 +9,27 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    -- diagnostics.cspell,
-    -- formatting.prettier,
-    code_actions.eslint,
-    code_actions.eslint,
-    formatting.eslint,
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    diagnostics.flake8,
-  },
-  -- Format on save
-  on_attach = function(client)
-      if client.resolved_capabilities.document_formatting then
-          vim.cmd([[
-          augroup LspFormatting
-              autocmd! * <buffer>
-              autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-          augroup END
-          ]])
-      end
-  end,
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		-- diagnostics.cspell,
+		-- formatting.prettier,
+		code_actions.eslint,
+		code_actions.eslint,
+		formatting.eslint,
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		diagnostics.flake8,
+	},
+	-- Format on save
+	-- on_attach = function(client)
+	--     if client.server_capabilities.documentFormattingProvider then
+	--         vim.cmd([[
+	--         augroup LspFormatting
+	--             autocmd! * <buffer>
+	--             autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
+	--         augroup END
+	--         ]])
+	--     end
+	-- end,
+})
--- nvim/lua/mulholo/nvim-tree.lua
+++ nvim/lua/mulholo/nvim-tree.lua
@@ -1,81 +1,83 @@
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-  return
+	return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup {
+nvim_tree.setup({
   disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 60,
-    height = 30,
-    hide_root_folder = false,
-    side = "right",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "<CR>" }, cb = tree_cb "edit" },
-        { key = "s", cb = tree_cb "vsplit" },
-        { key = "i", cb = tree_cb "split" },
-      },
-    },
-    number = false,
-    relativenumber = false,
-  },
-}
+	hijack_netrw = true,
+	open_on_setup = false,
+	ignore_ft_on_setup = {
+		"startify",
+		"dashboard",
+		"alpha",
+	},
+	open_on_tab = false,
+	hijack_cursor = false,
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 60,
+		hide_root_folder = false,
+		side = "right",
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "<CR>" }, cb = tree_cb("edit") },
+				{ key = "s", cb = tree_cb("vsplit") },
+				{ key = "i", cb = tree_cb("split") },
+			},
+		},
+		number = false,
+		relativenumber = false,
+	},
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+})
